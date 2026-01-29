afb7462438c9a96485695d9f870e6eb37d537f19
Andres Evans <andres_evans@outlook.com>
2026-01-29 02:58:38 -0500

    agregado el logour en menu hamburguesa

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -2,24 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { SolicitudesService } from '../../../core/services/solicitudes.service';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [RouterLink, MatButtonModule, MatMenuModule, MatIconModule, MatDivider],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
   private auth = inject(AuthService);
+  private router = inject(Router);
   private solicitudesService = inject(SolicitudesService);
 
   isLoggedIn = false;
   userName: string | null = null;
 
   hasSolicitud = false;
+
+  async logout() {
+    await this.auth.signOut();
+    this.auth.setCachedUserName(null);
+
+    this.isLoggedIn = false;
+    this.userName = null;
+    this.hasSolicitud = false;
+
+    await this.router.navigate(['/login']);
+    
+  }
+
 
   async ngOnInit() {
     //  1. Pintar inmediatamente desde cache (UX perfecta)
